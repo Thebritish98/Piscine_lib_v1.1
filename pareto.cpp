8e1034aa9b4a85e0369a7c7481eafd411f26a74e
Kalmuns <49645027+Kalmuns@users.noreply.github.com>
2019-04-17 15:33:50 +0200

    code_core v0.2.5 (Ruben edition)

---
+++ pareto.cpp
@@ -0,0 +1,30 @@
+#include  "main.h"
+
+
+
+
+
+
+
+
+//  void pareto_verif( vector< Path* > chemin_possible ) // Algo qui efface tous les chemins qui ne sont pas pareto optimo
+//{
+//    int i,y;
+//    for(i=0;i<chemin_possible.size;i++)
+//    {
+//        for(y=0;y<chemin_possible;y++)
+//        {
+//            if(y!=i)
+//           {
+//
+//               if(chemin_possible[i]->cout_tot1<chemin_possible[y]->cout_tot1&&chemin_possible[i]->cout_tot2<=chemin_possible[y]->cout_tot2)
+//                    chemin_possible[i].erase;
+//               else if (chemin_possible[i]->cout_tot1<=chemin_possible[y]->cout_tot1&&chemin_possible[i]->cout_tot2<chemin_possible[y]->cout_tot2)
+//                          chemin_possible[i].erase;
+//           }
+//        }
+//
+//    }
+//}
+
+
